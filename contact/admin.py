d9370c6342447d66187688029fb6d501a3c1c6c4
Aragaojorge <3glasser@gmail.com>
2023-11-07 16:05:44 -0300

    Creating table with contact data, the form and search

--- contact/admin.py
+++ contact/admin.py
@@ -7,13 +7,14 @@ from contact import models
 class ContactAdmin(admin.ModelAdmin):
     # Variables below will improve Django administration area exibition mode
     # They are tuples
-    list_display = 'id', 'first_name', 'last_name', 'phone',
-    ordering = 'id',
-    list_filter = 'created_date',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
+    # the order of the contacts will be descending
+    ordering = '-id',
+    # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 50
-    list_editable = 'first_name', 'last_name',
+    list_editable = 'first_name', 'last_name', 'show',
     list_display_links = 'id', 'phone',
     
 @admin.register(models.Category)
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,16 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 # Create your views here.
 def index(request):
     
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+    
+    # print(contacts.query)
     
     context = {
         'contacts': contacts,
+        'site_title': 'Contacts - '
     }
     
     return render(
         request,
         'contact/index.html',
+        context,
+    )
+    
+def search(request):
+    
+    # search_value will receive url
+    search_value = request.GET.get('q', '').strip()
+    
+    contacts = Contact.objects \
+    .filter(show=True) \
+    .filter(
+        Q(first_name__icontains=search_value) |
+        Q(last_name__icontains=search_value) |
+        Q(phone__icontains=search_value) |
+        Q(email__icontains=search_value)
+    )\
+    .order_by('-id')
+    
+    # print(contacts.query)
+    # print(search_value)
+    
+    # if search is empty, return to index.html
+    if search_value == '':
+        return redirect('contact:index')
+    
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - '
+    }
+    
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+    
+# this viwe will show a single contact in template contact.html
+# URL will give it a contact_id and this value will be used in the query
+def contact(request, contact_id):
+    
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    
+    # site_title will be used to show the contat name on web page title
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+    
+    context = {
+        'contact': single_contact,
+        'site_title': site_title,
+    }
+    
+    return render(
+        request,
+        # it will be rendered the contact.html template
+        'contact/contact.html',
         context,
     )
