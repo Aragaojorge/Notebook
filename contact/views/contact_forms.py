2bff8a37f4c79962b22ee67e13bb8dfe3f695839
Aragaojorge <3glasser@gmail.com>
2023-11-09 17:16:30 -0300

    Create form non field errors

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,4 +1,6 @@
+from typing import Any
 from django import forms
+from django.core.exceptions import ValidationError
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
@@ -9,6 +11,12 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ('first_name', 'last_name', 'phone')
+        
+    # It is called before save data in database 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        self.add_error('first_name', ValidationError('Mensagem de erro', code='invalid'))
+        return super().clean()
 
 # Create your CrUD views here.
 def create(request):
