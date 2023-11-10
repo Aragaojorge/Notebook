2845afa664e4af527a0df4c66585bf00410fa496
Aragaojorge <3glasser@gmail.com>
2023-11-10 19:20:13 -0300

    Created other fields

---
+++ contact/forms.py
@@ -0,0 +1,56 @@
+from collections.abc import Mapping
+from typing import Any
+from django import forms 
+from django.core.exceptions import ValidationError
+from django.core.files.base import File
+from django.db.models.base import Model
+from django.forms.utils import ErrorList
+from . import models
+
+# Create your form here
+class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'class-a class-b', 'placeholder': 'Aqui veio do init',
+                }
+            ),
+        )
+    
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+    
+    class Meta:
+        model = models.Contact
+        fields = ('first_name', 'last_name', 'phone', 'email', 'description', 'category')
+        
+    # It is called before save data in database 
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid'
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+        
+        
+        return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Veio do add_error',
+                    code='invalid'
+                )
+            )
+        return first_name
+    
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,31 +1,24 @@
-from typing import Any
-from django import forms
-from django.core.exceptions import ValidationError
-from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404, redirect
-from contact.models import Contact
-from django.db.models import Q
-
-# Create your form here
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ('first_name', 'last_name', 'phone')
-        
-    # It is called before save data in database 
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        self.add_error('first_name', ValidationError('Mensagem de erro', code='invalid'))
-        return super().clean()
+from typing import Any, Dict
+from django.shortcuts import render, redirect
+from contact.forms import ContactForm
 
 # Create your CrUD views here.
 def create(request):
     
     # if any field is not empty and send is pressed we send data
     if request.method == 'POST':
+        
+        form = ContactForm(request.POST)
+        
         context = {
-            'form': ContactForm(request.POST)
+            'form': form
         }
+        
+        if form.is_valid():
+            print('form is valid')
+            form.save()
+            return redirect('contact:create')
+        
         return render(
             request,
             'contact/create.html',
