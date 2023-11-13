7dfc14e9e41fbb88bf7404775b858e0e80e49e4d
Aragaojorge <3glasser@gmail.com>
2023-11-13 17:21:57 -0300

    Created update function and links to update and delete contact

--- contact/urls.py
+++ contact/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     
     # URL's for contact(CRUD)
     path('contact/<int:contact_id>/detail', views.contact, name='contact'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('contact/create/', views.create, name='create'),    
 ]
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,9 +1,13 @@
 from typing import Any, Dict
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from contact.forms import ContactForm
+from django.urls import reverse
+from contact.models import Contact
 
 # Create your CrUD views here.
 def create(request):
+    
+    form_action = reverse('contact:create')
     
     # if any field is not empty and send is pressed we send data
     if request.method == 'POST':
@@ -11,13 +15,14 @@ def create(request):
         form = ContactForm(request.POST)
         
         context = {
-            'form': form
+            'form': form,
+            'form_action': form_action,
         }
         
         if form.is_valid():
             print('form is valid')
-            form.save()
-            return redirect('contact:create')
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
         
         return render(
             request,
@@ -27,7 +32,47 @@ def create(request):
     
     # if fields are empty we will see the empty form again
     context = {
-        'form': ContactForm()
+        'form': ContactForm(),
+        'form_action': form_action,
+    }
+    
+    return render(
+        request,
+        'contact/create.html',
+        context,
+    )
+    
+def update(request, contact_id):
+    
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    
+    form_action = reverse('contact:update', args=(contact_id,))
+    
+    # if any field is not empty and send is pressed we send data
+    if request.method == 'POST':
+        
+        form = ContactForm(request.POST, instance=contact)
+        
+        context = {
+            'form': form,
+            'form_action': form_action,
+        }
+        
+        if form.is_valid():
+            print('form is valid')
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
+        
+        return render(
+            request,
+            'contact/create.html',
+            context,
+        )
+    
+    # if fields are empty we will see the empty form again
+    context = {
+        'form': ContactForm(instance=contact),
+        'form_action': form_action,
     }
     
     return render(
